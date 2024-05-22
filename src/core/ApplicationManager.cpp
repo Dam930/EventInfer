30c0fbbbacf0f69327b9f293ec11a1f9d9c2691c
Stefano Fiorentino <stefano.fiorentino@adesso.ch>
2024-05-22 21:20:39 +0000

    dump
    Signed-off-by: Stefano Fiorentino <stefano.fiorentino@adesso.ch>

--- src/core/ApplicationManager.cpp
+++ src/core/ApplicationManager.cpp
@@ -27,7 +27,7 @@ void ApplicationManager::run_app() {
         nlohmann::json json_camera_reader = {};
         nlohmann::json json_feats_calculator = {
             {"max_queue_element", 6},
-            {"onnx_model_path", "/workspaces/inference-cpp/res2net50_14w_8s_Opset16.onnx"}};
+            {"onnx_model_path", "/workspaces/inference-cpp/resnet50_v1.onnx"}};
 
         // Create instances
         std::unique_ptr<components::BaseComponent> agent_1 =
